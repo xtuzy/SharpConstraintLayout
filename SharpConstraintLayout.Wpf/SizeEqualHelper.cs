a545e6805b47bb087b86ecfa9dd4b63334a244a8
杨 周 <1570571499@qq.com>
2021-12-30 18:27:08 +0800

    obsolete all ConstraintSet api, optimize chained call method

---
+++ SharpConstraintLayout.Wpf/SizeEqualHelper.cs
@@ -0,0 +1,14 @@
+﻿using androidx.constraintlayout.core.widgets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpConstraintLayout.Wpf
+{
+    internal class SizeEqualHelper:HelperWidget
+    {
+        override 
+    }
+}
