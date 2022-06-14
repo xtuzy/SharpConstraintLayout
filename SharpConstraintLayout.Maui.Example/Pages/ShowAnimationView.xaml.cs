50df820da941862c5724264871efba02f88bfcd6
杨 周 <1570571499@qq.com>
2022-06-14 21:35:32 +0800

    add a simple animation api

--- SharpConstraintLayout.Maui.Example/Pages/ShowAnimationView.xaml.cs
+++ SharpConstraintLayout.Maui.Example/Pages/ShowAnimationView.xaml.cs
@@ -1,4 +1,5 @@
 using SharpConstraintLayout.Maui.Widget;
+using static SharpConstraintLayout.Maui.Widget.FluentConstraintSet;
 
 namespace SharpConstraintLayout.Maui.Example.Pages
 {
@@ -6,6 +7,8 @@ namespace SharpConstraintLayout.Maui.Example.Pages
     public partial class ShowAnimationView : ContentView
     {
         private Button button1;
+        private Button button2;
+        private ContentView backgroundView;
         private ConstraintLayout layout;
 
         public ShowAnimationView()
@@ -15,41 +18,53 @@ namespace SharpConstraintLayout.Maui.Example.Pages
             layout = new ConstraintLayout();
             this.Content = layout;
             button1 = new Button() { Text = "Button1" };
-            layout.Add(button1);
-            button1.Clicked += Button_Clicked;
-            using (var startset = new FluentConstraintSet())
-            {
-                startset.Clone(layout);
-                startset.Select(button1).CenterYTo().LeftToLeft();
-                startset.ApplyTo(layout);
-            }
+            button1.Clicked += Button1_Clicked;
+            button2 = new Button() { Text = "Button2" };
+            button2.Clicked += Button2_Clicked;
+            backgroundView = new ContentView() { BackgroundColor = Colors.Green };
+            layout.AddElement(backgroundView, button1, button2);
+
+            var startset = new FluentConstraintSet();
+            startset.Clone(layout);
+            startset.Select(button1).CenterYTo().LeftToLeft()
+                .Select(backgroundView).TopToTop().EdgesXTo().Width(SizeBehavier.MatchConstraint).PercentHeight(0.5f).Height(SizeBehavier.MatchConstraint)
+                .Select(button2).BottomToBottom(backgroundView, 20).RightToRight(backgroundView, 20);
+            startset.ApplyTo(layout);
+
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
+        bool isExpaned = true;
+        private void Button2_Clicked(object sender, EventArgs e)
         {
-            //button.ScaleTo(1.5, 1000, Easing.CubicIn);
-            //var animation = new Animation(v => button1.Scale = v, 1, 2);
-            //animation.Commit(this, "SimpleAnimation", 16, 2000, Easing.Linear, (v, c) => button1.Scale = 1, () => false);
-            var animation2 = new Animation(v =>
+            layout.AbortAnimation("ConstrainTo");
+            if (isExpaned)//ÐèÒªÊÕËõ
             {
-                using (var set = new FluentConstraintSet())
-                {
-                    set.Clone(layout);
-                    set.Select(button1).CenterTo().XBias((float)v).Rotation((float)v * 360).Scale((float)v * 2).Alpha((float)v)
-                        ;
-                    set.ApplyTo(layout);
-                }
-            }, 0, 1);
-            animation2.Commit(this, "SimpleAnimation", 16, 1000, Easing.CubicInOut, (v, c) =>
+                var finish = new FluentConstraintSet();
+                finish.Clone(layout);
+                finish.Select(backgroundView).Clear().TopToTop().LeftToLeft().Height(20).Width(SizeBehavier.MatchParent)
+                    .Select(button2).Clear().BottomToBottom(null, 20).RightToRight(null, 20);
+                layout.LayoutToWithAnim(finish, "ConstrainTo", 16, 1200, Easing.SpringOut);
+            }
+            else
             {
-                using (var finalSet = new FluentConstraintSet())
-                {
-                    finalSet.Clone(layout);
-                    finalSet.Select(button1).CenterYTo().RightToRight();
-                    finalSet.ApplyTo(layout);
-                }
-            }, () => false);
+                var start = new FluentConstraintSet();
+                start.Clone(layout);
+                start.Select(backgroundView).Clear().TopToTop().EdgesXTo().Width(SizeBehavier.MatchConstraint).PercentHeight(0.5f).Height(SizeBehavier.MatchConstraint)
+                    .Select(button2).Clear().BottomToBottom(backgroundView, 20).RightToRight(backgroundView, 20);
+                layout.LayoutToWithAnim(start, "ConstrainTo", 16, 3000, Easing.SpringOut);
+            }
+            isExpaned = !isExpaned;
+        }
 
+        private void Button1_Clicked(object sender, EventArgs e)
+        {
+            var start = new FluentConstraintSet();
+            start.Clone(layout);
+            start.Select(button1).Clear().CenterYTo().LeftToLeft();
+            var finish = new FluentConstraintSet();
+            finish.Clone(layout);
+            finish.Select(button1).Clear().CenterYTo().RightToRight();
+            layout.LayoutToWithAnim(finish, "ConstrainTo", 16, 1200, Easing.SpringOut, (v, b) => { start.ApplyTo(layout); });
         }
     }
 }
---
+++ SharpConstraintLayout.Maui/Widget/ViewInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpConstraintLayout.Maui.Widget
+{
+    public class ViewInfo
+    {
+        public double X;
+        public double Y;
+        public Size Size;
+        public double TranlateX;
+        public double TranlateY;
+        public double ScaleX;
+        public double ScaleY;
+        public double Alpha;
+        public double RotationX;
+        public double RotationY;
+    }
+}
